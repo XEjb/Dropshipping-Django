9fad2547c2631dde60abc730e895428e3294c63f
Xejb <vladiq-artamonenko@rambler.ru>
2023-03-01 21:12:40 +0300

    tests

--- shop/models.py
+++ shop/models.py
@@ -1,5 +1,10 @@
+from decimal import Decimal
+
 from django.contrib.auth.models import User
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.utils import timezone
 
 
 class Product(models.Model):
@@ -53,6 +58,30 @@ class Order(models.Model):
     def __str__(self):
         return f'{self.user} - {self.amount} - {self.status}'
 
+    @staticmethod
+    def get_cart(user: User):
+        cart = Order.objects.filter(user=user,
+                                    status=Order.STATUS_CART
+                                    ).first()
+        if cart and (timezone.now() - cart.creation_time).days > 7:
+            cart.delete()
+            cart = None
+
+        if not cart:
+            cart = Order.objects.create(user=user,
+                                        status=Order.STATUS_CART,
+                                        amount=0)
+        return cart
+
+    def get_amount(self):
+        amount = Decimal()
+        for item in self.orderitem_set.all():
+            amount += item.amount
+        return amount
+
+    def make_order(self):
+        pass
+
 
 class OrderItem(models.Model):
     order = models.ForeignKey(Order, on_delete=models.CASCADE)
@@ -66,3 +95,21 @@ class OrderItem(models.Model):
 
     def __str__(self):
         return f'{self.product} - {self.price}'
+
+    @property
+    def amount(self):
+        return self.quantity * (self.price - self.discount)
+
+
+@receiver(post_save, sender=OrderItem)
+def recalculaate_order_amount_after_save(sender, instance, **kwargs):
+    order = instance.order
+    order.amount = order.get_amount()
+    order.save()
+
+
+@receiver(post_save, sender=OrderItem)
+def recalculaate_order_amount_after_delete(sender, instance, **kwargs):
+    order = instance.order
+    order.amount = order.get_amount()
+    order.save()
