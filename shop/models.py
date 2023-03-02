57e25ca1db7339d45935dc70f3045fc3f6db39a8
Xejb <vladiq-artamonenko@rambler.ru>
2023-03-02 14:41:04 +0300

    writing models for tests, autopay function

--- shop/models.py
+++ shop/models.py
@@ -1,8 +1,9 @@
 from decimal import Decimal
 
 from django.contrib.auth.models import User
-from django.db import models
-from django.db.models.signals import post_save
+from django.db import models, transaction
+from django.db.models import Sum
+from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 from django.utils import timezone
 
@@ -19,7 +20,7 @@ class Product(models.Model):
         ordering = ['pk']
 
     def __str__(self):
-        return f'{self.name} - {self.price}'
+        return f'{self.name} - {self.price} '
 
 
 class Payment(models.Model):
@@ -32,7 +33,12 @@ class Payment(models.Model):
         ordering = ['pk']
 
     def __str__(self):
-        return f'{self.user} - {self.amount}'
+        return f'{self.user} - {self.amount} '
+
+    @staticmethod
+    def get_balance(user: User):
+        amount = Payment.objects.filter(user=user).aggregate(Sum('amount'))['amount__sum']
+        return amount or Decimal(0)
 
 
 class Order(models.Model):
@@ -56,7 +62,7 @@ class Order(models.Model):
         ordering = ['pk']
 
     def __str__(self):
-        return f'{self.user} - {self.amount} - {self.status}'
+        return f'{self.user} - {self.amount} - {self.status} '
 
     @staticmethod
     def get_cart(user: User):
@@ -74,13 +80,24 @@ class Order(models.Model):
         return cart
 
     def get_amount(self):
-        amount = Decimal()
+        amount = Decimal(0)
         for item in self.orderitem_set.all():
             amount += item.amount
         return amount
 
     def make_order(self):
-        pass
+        items = self.orderitem_set.all()
+        if items and self.status == Order.STATUS_CART:
+            self.status = Order.STATUS_WAITING_FOR_PAYMENT
+            self.save()
+            auto_payment_unpaid_orders(self.user)
+
+    @staticmethod
+    def get_amount_of_unpaid_orders(user: User):
+        amount = Order.objects.filter(user=user,
+                                      status=Order.STATUS_WAITING_FOR_PAYMENT,
+                                      ).aggregate(Sum('amount'))['amount__sum']
+        return amount or Decimal(0)
 
 
 class OrderItem(models.Model):
@@ -94,22 +111,42 @@ class OrderItem(models.Model):
         ordering = ['pk']
 
     def __str__(self):
-        return f'{self.product} - {self.price}'
+        return f'{self.product} - {self.price} '
 
     @property
     def amount(self):
         return self.quantity * (self.price - self.discount)
 
 
+@transaction.atomic()
+def auto_payment_unpaid_orders(user: User):
+    unpaid_orders = Order.objects.filter(user=user,
+                                         status=Order.STATUS_WAITING_FOR_PAYMENT)
+    for order in unpaid_orders:
+        if Payment.get_balance(user) < order.amount:
+            break
+        order.payment = Payment.objects.all().last()
+        order.status = Order.STATUS_PAID
+        order.save()
+        Payment.objects.create(user=user,
+                               amount=-order.amount)
+
+
 @receiver(post_save, sender=OrderItem)
-def recalculaate_order_amount_after_save(sender, instance, **kwargs):
+def recalculate_order_amount_after_save(sender, instance, **kwargs):
     order = instance.order
     order.amount = order.get_amount()
     order.save()
 
 
-@receiver(post_save, sender=OrderItem)
-def recalculaate_order_amount_after_delete(sender, instance, **kwargs):
+@receiver(post_delete, sender=OrderItem)
+def recalculate_order_amount_after_delete(sender, instance, **kwargs):
     order = instance.order
     order.amount = order.get_amount()
     order.save()
+
+
+@receiver(post_save, sender=Payment)
+def auto_payment(sender, instance, **kwargs):
+    user = instance.user
+    auto_payment_unpaid_orders(user)
